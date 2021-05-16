cbc5dc9dae56a2841aab3c539c761fc2b37daba6
krishna <krishna.nature91@gmail.com>
2021-05-16 15:34:05 +0100

    Source Maps for debugger

--- webpack-typescript/src/index.ts
+++ webpack-typescript/src/index.ts
@@ -1,2 +1,9 @@
-console.log('Hello');
-console.log('Hello again');
+import { formData } from "./forms";
+
+const form = document.querySelector('form')!;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = formData(form);
+  console.log('Hello: ', data);
+})
