de07ef05a713c128afcade0db93f66e417e203f1
krishna <krishna.nature91@gmail.com>
2021-05-16 14:39:47 +0100

    Setup webpack config file

---
+++ webpack-typescript/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules:[
+      {
+        test: /\.ts$/,  //Regex
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')]
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  }
+}
