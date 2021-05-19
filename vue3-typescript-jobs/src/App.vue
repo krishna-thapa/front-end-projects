d7612f2e458ce8a4274a89bc618c66c6b5f12f8b
krishna <krishna.nature91@gmail.com>
2021-05-19 22:11:30 +0100

    With the options API using TypeScript

--- vue3-typescript-jobs/src/App.vue
+++ vue3-typescript-jobs/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="app">
-    Hello, job hunters!
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Lugi')">Change name</button>
+    <button @click="changeAge('Thirty')">Change age</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Mario" as string,
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+      return age;
+    }
+  },
 });
 </script>
 
