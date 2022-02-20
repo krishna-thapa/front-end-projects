ce929a5379bbcd2d3a8d138105e696743acf452f
krishna-thapa <krishna.nature91@gmail.com>
2022-02-20 12:25:36 +0000

    Moved svlete to front-end projects

---
+++ svelte-intro/src/components/PollList.svelte
@@ -0,0 +1,42 @@
+<script>
+  // import { onMount, onDestroy } from 'svelte';
+  import { fade, slide, scale } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
+  import PollStore from '../stores/PollStore.js'
+  import PollDetails from './PollDetails.svelte'
+  
+  // export let polls = [];
+
+  // // Using the store, will update when the data get updated in the store
+  // const unsub = PollStore.subscribe(data => {
+  //   polls = data;
+  // });
+
+  // onMount(() => {
+  //   console.log('Component is mounted');
+  // });
+
+  // onDestroy(() => {
+  //   console.log('component destroyed');
+  //   unsub();
+  // });
+
+  // Note: Instead of using export and destroy to remove sub, just use $PollStore
+  // svelte/transition should be only be used in html dom not in the custom doms
+</script>
+
+<div class="poll-list">
+  {#each $PollStore as poll (poll.id)}
+    <div in:fade out:scale|local animate:flip={{duration: 500}}>
+      <PollDetails {poll} />
+    </div>
+  {/each}
+</div>
+
+<style>
+  .poll-list{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+</style>
---
+++ vuejs-d3-demo/Readme.md
@@ -0,0 +1,7 @@
+## Use of D3 in Vuejs
+
+Simple demo to integrate [D3 library](https://d3js.org/) to [Vuejs framework](https://vuejs.org/)
+
+
+## Run 
+`npm run serve`
